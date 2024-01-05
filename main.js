25740014e8d4c66844628cda5afef725b4f3224d
unknown <jaber.ahmed4770@gmail.com>
2024-01-05 20:01:14 +0200

    js basic practice

---
+++ main.js
@@ -0,0 +1,34 @@
+console.log("js added")
+
+
+let bottleColor = "Transparent";
+var waterContainer = 1;
+const waterAvailable = true;
+
+console.log(bottleColor, waterAvailable, waterContainer);
+
+
+
+let pcComponent = ["laptop", "mouse", "monitor", "ups", "keyboard", "CPU", "speaker"];
+
+console.log(pcComponent);
+console.log(pcComponent[1]);
+pcComponent[1] = "wireless mouse";
+console.log(pcComponent[1]);
+pcComponent.push("cable");
+console.log(pcComponent);
+pcComponent.pop();
+console.log(pcComponent);
+
+pcComponent.shift();
+console.log(pcComponent);
+
+pcComponent.unshift("multiplug");
+console.log(pcComponent);
+console.log(pcComponent.indexOf("ups"));
+
+
+
+
+
+
