31eb65c0a88955c64f69366b08baee8b6df8633e
unknown <jaber.ahmed4770@gmail.com>
2024-01-08 23:43:16 +0200

    assignment oparators

--- main.js
+++ main.js
@@ -58,3 +58,36 @@ let text1 = "A";
 let text2 = "B";
 let result = text1 > text2;
 console.log(result);
+
+
+
+/*
+
+    JavaScript Assignment Operators
+Assignment operators assign values to JavaScript variables.
+
+Operator	Example	Same As
+=	x = y	x = y
++=	x += y	x = x + y
+-=	x -= y	x = x - y
+*=	x *= y	x = x * y
+/=	x /= y	x = x / y
+%=	x %= y	x = x % y
+**=	x **= y	x = x ** y
+Shift Assignment Operators
+Operator	Example	Same As
+<<=	x <<= y	x = x << y
+>>=	x >>= y	x = x >> y
+>>>=	x >>>= y	x = x >>> y
+Bitwise Assignment Operators
+Operator	Example	Same As
+&=	x &= y	x = x & y
+^=	x ^= y	x = x ^ y
+|=	x |= y	x = x | y
+Logical Assignment Operators
+Operator	Example	Same As
+&&=	x &&= y	x = x && (x = y)
+||=	x ||= y	x = x || (x = y)
+??=	x ??= y	x = x ?? (x = y)
+
+*/
